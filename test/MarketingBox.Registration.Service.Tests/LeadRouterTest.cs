4e4ea5ee84e3cb4ec0ef51011ae4341a076b7ae6
Aleksandr B <aleksandr.b@smplt.net>
2021-11-01 14:15:33 +0400

    fised tests

--- test/MarketingBox.Registration.Service.Tests/LeadRouterTest.cs
+++ test/MarketingBox.Registration.Service.Tests/LeadRouterTest.cs
@@ -1,8 +1,13 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using MarketingBox.Affiliate.Service.Domain.Models.CampaignBoxes;
 using MarketingBox.Affiliate.Service.MyNoSql.CampaignBoxes;
+using MarketingBox.Registration.Service.MyNoSql.LeadRouter;
 using MarketingBox.Registration.Service.Services;
+using MyNoSqlServer.Abstractions;
 using NUnit.Framework;
 
 namespace MarketingBox.Registration.Service.Tests
@@ -88,12 +93,20 @@ namespace MarketingBox.Registration.Service.Tests
                 Sequence = 0,
             };
 
-            var leadRouter = new LeadRouter(1, 0,new CampaignBoxNoSql[]
+            var leadCounter = new FakeMyNoSqlReaderWriter<LeadRouterNoSqlEntity>();
+            var capacitor = new FakeMyNoSqlReaderWriter<LeadRouterCapacitorBoxNoSqlEntity>();
+
+            var leadRouter = new LeadRouter("default-tenant-id", 0, new CampaignBoxNoSql[]
             {
                 campaignBoxNoSql1,
                 campaignBoxNoSql2,
                 campaignBoxNoSql3,
-            }, leadRepository);
+            },
+                leadRepository,
+                leadCounter,
+                leadCounter,
+                capacitor,
+                capacitor);
 
             var campaignBox1 = await leadRouter.GetCampaignBox("CH");
             var campaignBox2 = await leadRouter.GetCampaignBox("CH");
@@ -107,6 +120,206 @@ namespace MarketingBox.Registration.Service.Tests
             Assert.AreEqual(campaignBoxNoSql1, campaignBox1_1);
             Assert.AreEqual(campaignBoxNoSql1, campaignBox1_2);
             Assert.AreEqual(campaignBoxNoSql1, campaignBox1_3);
+        }
+    }
+
+    public class FakeMyNoSqlReaderWriter<T> :
+        IMyNoSqlServerDataReader<T>, 
+        IMyNoSqlServerDataWriter<T> 
+        where T : MyNoSqlDbEntity, new()
+    {
+        public Dictionary<string, Dictionary<string, T>> Storage { get; set; }
+            = new();
+        public T Get(string partitionKey, string rowKey)
+        {
+            if (!Storage.TryGetValue(partitionKey, out var row))
+            {
+                return null;
+            }
+
+            if (row == null)
+                return null;
+
+            if (!row.TryGetValue(rowKey, out var val))
+            {
+                return null;
+            }
+
+            return val;
+        }
+
+        public IReadOnlyList<T> Get(string partitionKey)
+        {
+            if (!Storage.TryGetValue(partitionKey, out var row))
+            {
+                return null;
+            }
+
+            return row?.Values.ToArray();
+        }
+
+        public IReadOnlyList<T> Get(string partitionKey, int skip, int take)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IReadOnlyList<T> Get(string partitionKey, int skip, int take, Func<T, bool> condition)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IReadOnlyList<T> Get(string partitionKey, Func<T, bool> condition)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IReadOnlyList<T> Get(Func<T, bool> condition = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Count()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Count(string partitionKey)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Count(string partitionKey, Func<T, bool> condition)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IMyNoSqlServerDataReader<T> SubscribeToUpdateEvents(Action<IReadOnlyList<T>> updateSubscriber, Action<IReadOnlyList<T>> deleteSubscriber)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ValueTask InsertAsync(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ValueTask InsertOrReplaceAsync(T entity)
+        {
+            var partition = entity.PartitionKey;
+            var row = entity.RowKey;
+
+            if (Storage.TryGetValue(partition, out var stor))
+            {
+                stor[row] = entity;
+                return ValueTask.CompletedTask;
+            }
+
+            var dict = new Dictionary<string, T>();
+            dict[row] = entity;
+            Storage[partition] = dict;
+
+            return ValueTask.CompletedTask;
+        }
+
+        public ValueTask CleanAndKeepLastRecordsAsync(string partitionKey, int amount)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ValueTask BulkInsertOrReplaceAsync(IEnumerable<T> entity, DataSynchronizationPeriod dataSynchronizationPeriod = DataSynchronizationPeriod.Sec5)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ValueTask CleanAndBulkInsertAsync(IEnumerable<T> entity, DataSynchronizationPeriod dataSynchronizationPeriod = DataSynchronizationPeriod.Sec5)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ValueTask CleanAndBulkInsertAsync(string partitionKey, IEnumerable<T> entity,
+            DataSynchronizationPeriod dataSynchronizationPeriod = DataSynchronizationPeriod.Sec5)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ValueTask<OperationResult> ReplaceAsync(string partitionKey, string rowKey, Func<T, bool> updateCallback,
+            DataSynchronizationPeriod syncPeriod = DataSynchronizationPeriod.Sec5)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ValueTask<OperationResult> MergeAsync(string partitionKey, string rowKey, Func<T, bool> updateCallback,
+            DataSynchronizationPeriod syncPeriod = DataSynchronizationPeriod.Sec5)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ValueTask<IEnumerable<T>> GetAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IAsyncEnumerable<T> GetAllAsync(int bulkRecordsCount)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ValueTask<IEnumerable<T>> GetAsync(string partitionKey)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ValueTask<T> GetAsync(string partitionKey, string rowKey)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ValueTask<IReadOnlyList<T>> GetMultipleRowKeysAsync(string partitionKey, IEnumerable<string> rowKeys)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ValueTask<T> DeleteAsync(string partitionKey, string rowKey)
+        {
+            if (Storage.TryGetValue(partitionKey, out var dict))
+            {
+                if (dict.Remove(rowKey, out var val))
+                {
+                    return ValueTask.FromResult(val);
+                }
+            }
+
+            return ValueTask.FromResult<T>(null);
+        }
+
+        public ValueTask<IEnumerable<T>> QueryAsync(string query)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ValueTask<IEnumerable<T>> GetHighestRowAndBelow(string partitionKey, string rowKeyFrom, int amount)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ValueTask CleanAndKeepMaxPartitions(int maxAmount)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ValueTask CleanAndKeepMaxRecords(string partitionKey, int maxAmount)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ValueTask<int> GetCountAsync(string partitionKey)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ValueTask<ITransactionsBuilder<T>> BeginTransactionAsync()
+        {
+            throw new NotImplementedException();
         }
     }
 }
