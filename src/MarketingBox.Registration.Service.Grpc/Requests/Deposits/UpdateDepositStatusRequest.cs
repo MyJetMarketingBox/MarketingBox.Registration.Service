768a7c0cec6ee570fefd64a132d80932aff10e75
Lev Kurbanov <lev.k@smplt.net>
2022-05-05 15:46:38 +0500

    added enum validation

--- src/MarketingBox.Registration.Service.Grpc/Requests/Deposits/UpdateDepositStatusRequest.cs
+++ src/MarketingBox.Registration.Service.Grpc/Requests/Deposits/UpdateDepositStatusRequest.cs
@@ -1,5 +1,6 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.Runtime.Serialization;
+using MarketingBox.Sdk.Common.Attributes;
 using MarketingBox.Sdk.Common.Enums;
 using MarketingBox.Sdk.Common.Models;
 
@@ -10,19 +11,19 @@ namespace MarketingBox.Registration.Service.Grpc.Requests.Deposits
     {
         [DataMember(Order = 1), Required, StringLength(128, MinimumLength = 1)]
         public string TenantId { get; set; }
-        
+
         [DataMember(Order = 2), Required, Range(1, long.MaxValue)]
         public long? UserId { get; set; }
 
-        [DataMember(Order = 3), Required, Range(1,long.MaxValue)]
+        [DataMember(Order = 3), Required, Range(1, long.MaxValue)]
         public long? RegistrationId { get; set; }
 
-        [DataMember(Order = 4), Required]
+        [DataMember(Order = 4), IsEnum, Required]
         public DepositUpdateMode? Mode { get; set; }
-        
-        [DataMember(Order = 5), Required]
+
+        [DataMember(Order = 5), IsEnum, Required]
         public RegistrationStatus? NewStatus { get; set; }
-        
+
         [DataMember(Order = 6), Required, StringLength(512, MinimumLength = 1)]
         public string Comment { get; set; }
     }
